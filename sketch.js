5443b8682aa08ff33ebf0e89c8495beec05225de
ProfFaHElo <168576374+ProfFaHElo@users.noreply.github.com>
2024-05-21 15:37:38 -0300

    adicionando arquivos

---
+++ sketch.js
@@ -0,0 +1,14 @@
+}
+
+function draw() {
+  
+  stroke("blue")
+  fill("red")
+  
+  //console.log(mouseIsPressed)
+  
+  if (mouseIsPressed) {
+   rect(mouseX,mouseY,20,35);   
+                                                                      }
+  
+}
